7adaf0df1929074b3b6416a16f526d4e1a5fece1
igurbev <igurbev@gmail.com>
2025-11-24 09:20:55 +0200

    meteo ride user

--- js/config.js
+++ js/config.js
@@ -26,7 +26,7 @@ export const mapTiles = {
     }
 };
 
-export const trackListUrl = "https://tracks.igurbev.net/api/tracklist/2";
+export const trackListUrl = "https://tracks.igurbev.net/api/tracklist/3";
 
 export const trackConf = {
     weight: 4,
